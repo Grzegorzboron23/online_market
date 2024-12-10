e078eb111e212a41db135439437295e91385c498
Grzegorz Boroń <grzegorzboron23@gmail.com>
2024-12-10 14:04:30 +0100

    refactor

--- src/main/java/com/solvd/onlinemarket/utils/AdvancedFileUploader.java
+++ src/main/java/com/solvd/onlinemarket/utils/AdvancedFileUploader.java
@@ -1,21 +1,25 @@
 package com.solvd.onlinemarket.utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class AdvancedFileUploader {
+    private static final Logger logger = LogManager.getLogger(AdvancedFileUploader.class);
 
     public static void uploadFile(String fileName) {
-        System.out.println("Uploading file: " + fileName);
+        logger.info("Uploading file: {}", fileName);
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("File " + fileName + " uploaded successfully!");
+        logger.info("File {} uploaded successfully!", fileName);
     }
 
 
     public static void performOtherActions() {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Performing rest actions " + i);
+            logger.info("Performing rest actions {}", i);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
